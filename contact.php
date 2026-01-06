1024c616d9c3e890228c6d70f85b61bbb2f6bbbc
erzahaxhimehmeti <haxhimehmetierza@gmail.com>
2026-01-06 22:57:14 +0100

    Add files via upload
    updated php file

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootcamp Gaming Zone</title>
+    <link rel="icon" type="image/x-icon" href="bootcamp foto/b1.jpg">
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body style="background-color: grey;">
+
+    <div class="navbar">
+        <div class="icon">
+            <h2 class="logo">Bootcamp</h2>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="finalproject.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="location.html">LOCATION</a></li>
+                <li><a href="contact.html">CONTACT </a></li>
+            </ul>
+        </div>
+
+        <div class="search">
+            <input class="srch" type="search" name="" placeholder="Type here">
+            <a href="#"> <button class="btn">Search</button></a>
+        </div>
+    </div>
+    
+    <h3>Below you will find our contact information!</h3>
+
+    <ol>
+        <li><a href="https://mail.google.com/mail/u/1/#inbox">Email</a></li>
+        <li><a href="https://www.instagram.com/bootcamp_gaming/">Instagram</a></li>
+        <li><a href="https://www.facebook.com/gentian.haxhimehmeti">Facebook</a></li>
+        <li><a href="#">Reservations: +38349173543</a></li>
+   </ol>
+</body>
+</html>
