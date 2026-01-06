130ed2444610b191313fdd67c3bd6a33a4d01bef
erzahaxhimehmeti <haxhimehmetierza@gmail.com>
2026-01-06 22:58:46 +0100

    Add files via upload
    php files that connect the webpage to the database on the backend

---
+++ classes/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db = "bootcamp_db";
+    private $user = "root";
+    private $pass = "";
+    protected $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db}",
+                $this->user,
+                $this->pass
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database connection failed");
+        }
+    }
+}
