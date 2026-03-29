934fd28cc7a38333f362667ae398f5adecf8013d
VleraZyhrani <vz73277@ubt-uni.net>
2026-03-29 19:00:28 +0200

    updated

--- register.php
+++ register.php
@@ -4,6 +4,21 @@ require_once 'classes/User.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+
+    /*var_dump($email, $password); i was debugging
+    exit; */
+} 
+
+$user = new User();
+$result = $user->register($email, $password);
+
+/*var_dump($result); e kom perdor per debug
+exit; */
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
 
@@ -12,28 +27,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (!preg_match($emailPattern, $email)) {
         $_SESSION['error'] = "Invalid email format";
-          header("Location: index.php");
+        header("Location: index.php");
         exit;
     }
 
     if (!preg_match($passwordPattern, $password)) {
-        $_SESSION['error'] = "Password must be 8+ chars, contain 1 uppercase letter and 1 number";
-          header("Location: index.php");
+        $_SESSION['error'] = "Password must be 8+ chars, include 1 uppercase letter and 1 number";
+        header("Location: index.php");
         exit;
     }
 
     $user = new User();
-    $user->register($email, $password);
+    $result = $user->register($email, $password);
 
     if (!$result) {
-        $_SESSION['error'] = "Email already registered";
+        $_SESSION['error'] = "Email already exists";
         header("Location: index.php");
         exit;
     }
 
     $_SESSION['success'] = "Registration successful!";
-
     header("Location: index.php");
     exit;
 }
+
+echo "User inserted!";
+exit;
+?>
 
