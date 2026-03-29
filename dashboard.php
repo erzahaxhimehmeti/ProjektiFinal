a40a91e3b1adcb6de3379da415d4f3d952028a5f
VleraZyhrani <vz73277@ubt-uni.net>
2026-03-29 18:58:21 +0200

    updated

--- dashboard.php
+++ dashboard.php
@@ -6,4 +6,8 @@ requireAdmin();
 <h1>Admin Dashboard</h1>
 <p>Welcome, <?php echo $_SESSION['email']; ?></p>
 
-<a href="logout.php">Logout</a>
+<ul>
+    <li><a href="add_news.php">Add News</a></li>
+    <li><a href="news.php">View News</a></li>
+    <li><a href="logout.php">Logout</a></li>
+</ul>
