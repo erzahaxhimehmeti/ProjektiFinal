bb8a9415a4e916e651ae234a43c1a3a0b4b77a28
VleraZyhrani <vz73277@ubt-uni.net>
2026-03-29 19:00:47 +0200

    updated

--- User.php
+++ User.php
@@ -1,26 +1,47 @@
+
 <?php
 require_once 'Database.php';
 
 class User extends Database {
 
-    public function register($email, $password) {
+  public function register($email, $password) {
+    try {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $sql = "SELECT id FROM users WHERE email = :email";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute([':email' => $email]);
-
-        if ($stmt->rowCount() > 0) {
-            return false;
-        }
-
-
-        $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+        $sql = "INSERT INTO users (email, password, role, created)
+         VALUES (:email, :password, 'user', NOW())";
         $stmt = $this->conn->prepare($sql);
 
         return $stmt->execute([
             ':email' => $email,
             ':password' => $hashedPassword
         ]);
+
+    } catch (PDOException $e) {
+        echo "DB ERROR: " . $e->getMessage();
+        exit;
     }
+}
+
+    
+
+    public function login($email, $password) {
+
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute([':email' => $email]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$user) {
+        return false;
+    }
+
+    if (!password_verify($password, $user['password'])) {
+        return false;
+    }
+
+    return $user;
+}
+
 }
